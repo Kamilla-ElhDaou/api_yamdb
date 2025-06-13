e785084a567a552acaa41dcda9b1669618597c1b
rafael <drypakcrew@gmail.com>
2025-06-14 02:07:22 +0300

    Добавил реги моделей в админку

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Category, Genre, Title
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name', 'slug')
+    ordering = ('name',)
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name', 'slug')
+    ordering = ('name',)
+
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year', 'category')
+    list_filter = ('year', 'category', 'genre')
+    search_fields = ('name', 'description')
+    ordering = ('name',)
