4749c61914941fbe381d17c5cf94948e17d6e6ca
rafael <drypakcrew@gmail.com>
2025-06-14 02:04:27 +0300

    Убрал лишние маршруты и импорты

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,9 +1,6 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView
-)
+
 from api.views import CategoryViewSet, GenreViewSet, TitleViewSet
 
 
@@ -14,6 +11,4 @@ router_v1.register('categories', CategoryViewSet, basename='categories')
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
-    path('v1/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair',),
-    path('v1/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh',),
 ]
