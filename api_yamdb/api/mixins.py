9b83fae7a5c755bf32fefd74ec03eef469c0887e
rafael <drypakcrew@gmail.com>
2025-06-14 02:02:06 +0300

    Добавил has_object_permission, исправил миксин в api/mixins.py

--- api_yamdb/api/mixins.py
+++ api_yamdb/api/mixins.py
@@ -1,1 +1,16 @@
-ы
+from rest_framework import mixins, viewsets
+
+
+class CreateListDestroyViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet
+):
+    """
+    Базовый вьюсет, предоставляет следующие действия:
+    - create(создание)
+    - list(получение списка)
+    - destroy(удаление)
+    Удобен для моделей, где не требуется обновление и детали.
+    """
